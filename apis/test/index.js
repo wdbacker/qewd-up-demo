83d52241d971ec00776527ba6021d8447f7f4948
Ward De Backer <wdb@stabe.be>
2021-10-29 12:20:53 +0200

    chore: add DocumentNode demo to handlers

--- apis/test/index.js
+++ apis/test/index.js
@@ -1,9 +1,18 @@
 module.exports = function(args, finished) {
   // this handler picks up incoming text for testing purposes
   let incomingText = args.req.query.text || ""
-  let d = new Date()
+  // create a DocumentNode reference to the ^QEWDTest global in IRIS
+  let qewdDemo = new this.documentStore.DocumentNode('QEWDDemo');
+  // assign the text content to the subscript 'demo' in this global
+  qewdDemo.$('demo').value = incomingText
+  // put the complete message JSON object inside a second level subscript
+  qewdDemo.$('demo').$('request').setDocument(args.req);
+  // read everything back in from IRIS into a JSON object
+  let qewdDemoGlobal = qewdDemo.$('demo').getDocument(true);
   // return the response to the client using WebSockets (or Ajax mode)
+  let d = new Date()
   finished({
-    text: 'You sent: "' + incomingText + '" to QEWD-Up at ' + d.toUTCString()
+    text: 'You sent: "' + incomingText + '" to QEWD-Up at ' + d.toUTCString(),
+    qewdDemoGlobal
   })
 }
